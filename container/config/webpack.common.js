c8a96cdc6d81e1bf6d0c3354914bbf8e2d2666e1
Hassam Ali <hassam.ali@gmail.com>
2021-06-01 21:23:44 +0500

    create container

---
+++ container/config/webpack.common.js
@@ -0,0 +1,19 @@
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { ModuleFederationPlugin } = require("webpack").container;
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
