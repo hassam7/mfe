afbe421aeb68fbba64b320e2a6415d0232483fa8
Hassam Ali <hassam.ali@gmail.com>
2021-06-04 23:02:58 +0500

    Add Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
-
+import Header from "./components/Header";
 
 export default () => {
   return (
     <div>
-      <MarketingApp />
-      <h1>Hi there</h1>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
