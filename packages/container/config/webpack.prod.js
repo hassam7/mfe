4444e0fa8ca150fd27a497da9b9a143cd1cb6bcd
Hassam Ali <hassam.ali@gmail.com>
2021-06-05 16:04:40 +0500

    add console.log for domain

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,7 +4,7 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
-
+console.log("DOMAIN IS: ", domain);
 const prodConfig = {
   mode: "production",
   output: {
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 import App from "./app";
 const mount = (el) => {
-  ReactDOM.render(<App />, el);
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history} />, el);
 };
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
