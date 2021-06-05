6a50bc18235fffcfb22761e947df4cc5677aee37
Hassam Ali <hassam.ali@gmail.com>
2021-06-05 18:04:42 +0500

    synchrozine history accross marketing and container

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createMemoryHistory } from "history";
 import App from "./app";
-const mount = (el) => {
+
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+  if (onNavigate) history.listen(onNavigate);
   ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate: ({ pathname: nextPathName }) => {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
+
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) mount(devRoot);
