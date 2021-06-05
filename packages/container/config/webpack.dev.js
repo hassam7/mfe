47dc8613120bc1dc771100c817cd3bd1f6728c09
Hassam Ali <hassam.ali@gmail.com>
2021-06-05 20:14:24 +0500

    update public path

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -4,6 +4,9 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
+  output: {
+    publicPath: "http://localhost:8080/",
+  },
   devServer: {
     port: 8080,
     historyApiFallback: {
