e820270adde01bbb56738b3e8caac2c2583c4dd5
Hassam Ali <hassam.ali@gmail.com>
2021-06-05 18:08:17 +0500

    use browser history when running in isolation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./app";
 
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
   if (onNavigate) history.listen(onNavigate);
   ReactDOM.render(<App history={history} />, el);
   return {
@@ -20,6 +20,7 @@ const mount = (el, { onNavigate }) => {
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
-  if (devRoot) mount(devRoot);
+  const history = createBrowserHistory();
+  if (devRoot) mount(devRoot, { defaultHistory: history});
 }
 export { mount };
