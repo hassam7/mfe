9f29eb0e311902a54dec26f3a24733b0efc1456b
Hassam Ali <hassam.ali@gmail.com>
2021-06-06 00:28:52 +0500

    lazy load marketing and auth

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
-import MarketingApp from "./components/MarketingApp";
+
+import Progress from "./components/Progress";
 import Header from "./components/Header";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({ productionPrefix: "co" });
 export default () => {
@@ -15,7 +19,12 @@ export default () => {
         <StylesProvider generateClassName={generateClassName}>
           <div>
             <Header />
-            <MarketingApp />
+            <Suspense fallback={<Progress />}>
+              <Switch>
+                <Route path="/auth" component={AuthLazy} />
+                <Route path="/" component={MarketingLazy} />
+              </Switch>
+            </Suspense>
           </div>
         </StylesProvider>
       </BrowserRouter>
