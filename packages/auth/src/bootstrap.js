061eec79248ed7a9a06385b490fed29076758423
Hassam Ali <hassam.ali@gmail.com>
2021-06-05 20:09:12 +0500

    add auth module

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./app";
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) history.listen(onNavigate);
+  ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate: ({ pathname: nextPathName }) => {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  const history = createBrowserHistory();
+  if (devRoot) mount(devRoot, { defaultHistory: history });
+}
+export { mount };
