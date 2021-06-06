543a6fdc86dbbb8ab2a108025a9fcfc188d9e1f6
Hassam Ali <hassam.ali@gmail.com>
2021-06-06 17:25:39 +0500

    add mount for dashboard

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,41 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createMemoryHistory, createBrowserHistory } from 'history';
-import App from './App';
+import { createApp } from "vue";
+
+import Dashboard from "./components/Dashboard.vue";
 
 // Mount function to start up the app
-const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
-  const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath],
-    });
-
-  if (onNavigate) {
-    history.listen(onNavigate);
-  }
-
-  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
-
-  return {
-    onParentNavigate({ pathname: nextPathname }) {
-      const { pathname } = history.location;
-
-      if (pathname !== nextPathname) {
-        history.push(nextPathname);
-      }
-    },
-  };
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
 };
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_auth-dev-root');
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_dashboard-dev-root");
 
-  if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() });
-  }
+  if (devRoot) mount(devRoot);
 }
 
 // We are running through container
